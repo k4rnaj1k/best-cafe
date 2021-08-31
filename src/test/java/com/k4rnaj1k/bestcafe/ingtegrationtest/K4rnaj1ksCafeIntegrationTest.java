051000a4571ba27bbd35d11212c83a6dd15813ab
k4rnaj1k <vladliasota@gmail.com>
2021-08-31 16:14:39 +0300

    Integration tests added, not yet written.

---
+++ src/test/java/com/k4rnaj1k/bestcafe/ingtegrationtest/K4rnaj1ksCafeIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.k4rnaj1k.bestcafe.ingtegrationtest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+public class K4rnaj1ksCafeIntegrationTest {
+    private WebTestClient webTestClient;
+
+//    public void testRegistration(){
+//        webTestClient.get().uri("/api/v1/")
+//    }
+}
