9b5c6bcdf2aabbabd9c3b66110510ff8ad8cf340
k4rnaj1k <vladliasota@gmail.com>
2021-09-15 13:12:36 +0300

    Added heroku url to readme.

--- src/main/java/com/k4rnaj1k/bestcafe/dto/menuitem/DishPostDTO.java
+++ src/main/java/com/k4rnaj1k/bestcafe/dto/menuitem/DishPostDTO.java
@@ -2,13 +2,14 @@ package com.k4rnaj1k.bestcafe.dto.menuitem;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public record DishPostDTO(
         @NotBlank
         String name,
-        @NotNull
+        @NotNull @NotEmpty
         List<Long> ingredients,
         @DecimalMin("1")
         Double price) {
