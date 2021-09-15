d78a6b64434a415af12fea7284d2ebe5379af72b
k4rnaj1k <vladliasota@gmail.com>
2021-09-15 13:14:08 +0300

    Merge with main.

--- src/main/java/com/k4rnaj1k/bestcafe/configuration/SpringDocConfig.java
+++ src/main/java/com/k4rnaj1k/bestcafe/configuration/SpringDocConfig.java
@@ -5,11 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
@@ -18,7 +15,6 @@ public class SpringDocConfig {
         return new OpenAPI().components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP)
                         .scheme("bearer")
                         .bearerFormat("JWT"))).addSecurityItem(new SecurityRequirement().addList("bearer-key"))
-                .info(new Info().title("k4rnaj1k's cafe").version("v1.0"))
-                .servers(List.of(new Server().url("http://localhost:8080/api/v1").description("local server"), new Server().url("https://best-cafe.herokuapp.com/api/v1").description("remote heroku server")));
+                .info(new Info().title("k4rnaj1k's cafe").version("v1.0").description("<a href=https://github.com/k4rnaj1k/best-cafe>Source code on github.</a>"));
     }
 }
